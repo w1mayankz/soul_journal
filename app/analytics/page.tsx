fec81be59166465fb015a76050ec5c1a13cab594
Mayank <w1mayankz@gmail.com>
2026-09-09 16:44:02 +0530

    Create page.tsx

---
+++ app/analytics/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { TopNavbar } from '../../components/top-navbar';
+
+export default function PlaceholderPage() {
+  return (
+    <div className="relative min-h-screen bg-black pb-24 font-sans text-white">
+      <TopNavbar />
+    </div>
+  );
+}
